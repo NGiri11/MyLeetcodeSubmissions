ab583da83ca6919b19e621f89ca980be51c87acd
NGiri11 <121069271+NGiri11@users.noreply.github.com>
2026-03-16 15:30:34 +0530

    Time: 0 ms (100%), Space: 94.9 MB (94.27%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> merged;
-        int i = 0, j = 0;
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] < nums2[j]) merged.push_back(nums1[i++]);
-            else merged.push_back(nums2[j++]);
+        if (nums1.size() > nums2.size()) return findMedianSortedArrays(nums2, nums1);
+
+        int m = nums1.size(), n = nums2.size();
+        int left = 0, right = m;
+        while (left <= right) {
+            int partitionA = (left + right) / 2;
+            int partitionB = (m + n + 1) / 2 - partitionA;
+
+            int maxLeftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+            int minRightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+            int maxLeftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+            int minRightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((m + n) % 2 == 0)
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+                else
+                    return max(maxLeftA, maxLeftB);
+            } else if (maxLeftA > minRightB) {
+                right = partitionA - 1;
+            } else {
+                left = partitionA + 1;
+            }
         }
-        while (i < nums1.size()) merged.push_back(nums1[i++]);
-        while (j < nums2.size()) merged.push_back(nums2[j++]);
-        int n = merged.size();
-        if (n % 2 == 0) return (merged[n/2 - 1] + merged[n/2]) / 2.0;
-        else return merged[n/2];
+        return 0.0;
     }
 };
