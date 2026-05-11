53761a88e9aa26a453f7b6d0583b651a887a3811
NGiri11 <121069271+NGiri11@users.noreply.github.com>
2026-05-11 16:31:05 +0530

    Time: 0 ms (100%), Space: 15.4 MB (48.01%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -3,13 +3,9 @@ public:
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> answer;
         for (int num : nums) {
-            vector<int> temp;
-            while (num > 0) {
-                temp.push_back(num % 10);
-                num /= 10;
-            }
-            for (int i = temp.size() - 1; i >= 0; --i) {
-                answer.push_back(temp[i]);
+            string s = to_string(num);
+            for (char c : s) {
+                answer.push_back(c - '0');
             }
         }
         return answer;
