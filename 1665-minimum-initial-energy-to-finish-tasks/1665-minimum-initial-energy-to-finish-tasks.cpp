42cca3e19a84870db24926bed17c67cb847b6c6d
NGiri11 <121069271+NGiri11@users.noreply.github.com>
2026-05-12 21:12:05 +0530

    Time: 41 ms (54.37%), Space: 97.2 MB (85.92%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [](const vector<int>& a, const vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+        int totalEnergy = 0;
+        int currentEnergy = 0;
+        for (const auto& task : tasks) {
+            int actual = task[0];
+            int minimum = task[1];
+            if (currentEnergy < minimum) {
+                int needed = minimum - currentEnergy;
+                totalEnergy += needed;
+                currentEnergy += needed;
+            }
+            currentEnergy -= actual;
+        }
+        return totalEnergy;
+    }
+};
