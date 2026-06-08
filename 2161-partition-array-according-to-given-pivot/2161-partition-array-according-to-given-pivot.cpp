9b0aa9bd80cbbf60d5984bb3fc0939ee63b34371
NGiri11 <121069271+NGiri11@users.noreply.github.com>
2026-06-08 09:43:41 +0530

    Time: 3 ms (87.42%), Space: 127.6 MB (88.18%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+        vector<int> result(n);
+        int left = 0;
+        int right = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < pivot) {
+                result[left++] = nums[i];
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] > pivot) {
+                result[right--] = nums[i];
+            }
+        }
+        while (left <= right) {
+            result[left++] = pivot;
+        }
+        return result;
+    }
+};
